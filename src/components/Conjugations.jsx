162b87854bb9e250a2e65488ba2befbbb8bc25cf
Chris Schreiner <chrisjschreiner@gmail.com>
2023-07-29 17:47:24 -0700

    changed number of columns

--- src/components/Conjugations.jsx
+++ src/components/Conjugations.jsx
@@ -1,9 +1,9 @@
 const Conjugations = ({ conjugations }) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 mt-4 gap-x-8 gap-y-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-4 gap-x-8 gap-y-4">
       {conjugations?.map((infinitive, index) => (
         <div className="mb-4 mr-2" key={index}>
-          <h2 className="text-lg font-bold">{infinitive.tense}</h2>
+          <h2 className="text-lg font-bold mb-2">{infinitive.tense}</h2>
           <p>Yo: {infinitive.yo}</p>
           <p>Tú: {infinitive.tú}</p>
           <p>Él/Ella/Usted: {infinitive["él/ella/usted"]}</p>
