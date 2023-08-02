ef92f66ad686ec30fadf5abf9b132015d98be9ae
Chris Schreiner <chrisjschreiner@gmail.com>
2023-08-02 09:15:24 -0700

    changed padding

--- src/components/Conjugations.jsx
+++ src/components/Conjugations.jsx
@@ -1,8 +1,8 @@
 const Conjugations = ({ conjugations }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 mt-4 gap-x-8 gap-y-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-4 gap-x-8 gap-y-4">
       {conjugations?.map((infinitive, index) => (
-        <div className="mb-4 mr-2" key={index}>
+        <div className="mb-4" key={index}>
           <h2 className="text-lg font-bold mb-2">{infinitive.tense}</h2>
           <table
             key={index}
@@ -10,38 +10,38 @@ const Conjugations = ({ conjugations }) => {
           >
             <thead className="text-left">
               <tr>
-                <th className="text-gray-700 font-medium rounded-l bg-blue-100 text-left px-7 py-2">
+                <th className="text-gray-700 font-medium rounded-l bg-blue-100 text-left px-6 py-2">
                   Pronoun
                 </th>
-                <th className="text-gray-700 font-medium rounded-r bg-blue-100 text-left px-7 py-2">
+                <th className="text-gray-700 font-medium rounded-r bg-blue-100 text-left px-6 py-2">
                   Conjugation
                 </th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="px-7 py-2">Yo</td>
-                <td className="px-7 py-2">{infinitive.yo}</td>
+                <td className="px-6 py-2">Yo</td>
+                <td className="px-6 py-2">{infinitive.yo}</td>
               </tr>
               <tr>
-                <td className="px-7 py-2">Tú</td>
-                <td className="px-7 py-2">{infinitive.tú}</td>
+                <td className="px-6 py-2">Tú</td>
+                <td className="px-6 py-2">{infinitive.tú}</td>
               </tr>
               <tr>
-                <td className="px-7 py-2">Él/Ella/Ud.</td>
-                <td className="px-7 py-2">{infinitive["él/ella/usted"]}</td>
+                <td className="px-6 py-2">Él/Ella/Ud.</td>
+                <td className="px-6 py-2">{infinitive["él/ella/usted"]}</td>
               </tr>
               <tr>
-                <td className="px-7 py-2">Nostros/as</td>
-                <td className="px-7 py-2">{infinitive["nosotros/as"]}</td>
+                <td className="px-6 py-2">Nostros/as</td>
+                <td className="px-6 py-2">{infinitive["nosotros/as"]}</td>
               </tr>
               <tr>
-                <td className="px-7 py-2">Vosotros/as</td>
-                <td className="px-7 py-2">{infinitive["vosotros/as"]}</td>
+                <td className="px-6 py-2">Vosotros/as</td>
+                <td className="px-6 py-2">{infinitive["vosotros/as"]}</td>
               </tr>
               <tr>
-                <td className="px-7 pt-2 pb-4">Ellos/Ellas/Uds.</td>
-                <td className="px-7 pt-2 pb-4">
+                <td className="px-6 pt-2 pb-4">Ellos/Ellas/Uds.</td>
+                <td className="px-6 pt-2 pb-4">
                   {infinitive["ellos/ellas/ustedes"]}
                 </td>
               </tr>
