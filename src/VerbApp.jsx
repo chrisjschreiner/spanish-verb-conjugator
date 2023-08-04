d35ea3ef7dc1d88c8ebdf801e7db80296033719c
Chris Schreiner <chrisjschreiner@gmail.com>
2023-08-04 08:14:54 -0700

    change title font size

--- src/VerbApp.jsx
+++ src/VerbApp.jsx
@@ -58,7 +58,7 @@ const VerbApp = () => {
 
   return (
     <div className="container mx-auto max-w-screen-md px-8 pt-16">
-      <h1 className="text-center text-5xl font-semibold pb-14">
+      <h1 className="text-center text-4xl md:text-5xl font-semibold pb-14">
         Spanish Verb Conjugator
       </h1>
       <form onSubmit={handleSubmit}>
