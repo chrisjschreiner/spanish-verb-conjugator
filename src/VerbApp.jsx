ebacc7db6cee2992fcb6aea427ca07218d52f1b4
Chris Schreiner <chrisjschreiner@gmail.com>
2023-08-13 06:51:25 -0700

    change path of json folder

--- src/VerbApp.jsx
+++ src/VerbApp.jsx
@@ -15,7 +15,7 @@ const VerbApp = () => {
 
   useEffect(() => {
     axios
-      .get("src/data/verbsData.json")
+      .get("verbsData.json")
       .then((res) => {
         setVerbsData(res.data);
       })
