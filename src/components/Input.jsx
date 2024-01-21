3e59768db538b7a30d4ff488e193dcb0b699b544
Chris Schreiner <chrisjschreiner@gmail.com>
2024-01-21 14:41:55 -0800

    create handleInputClick function to clear search field on click and reset autocomplete to true

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,6 +8,13 @@ const Input = ({
   open,
   setOpen,
 }) => {
+  const handleInputClick = () => {
+    setTimeout(() => {
+      setOpen(true);
+    });
+    setSearchVerb("");
+  };
+
   return (
     <>
       <input
@@ -16,6 +23,8 @@ const Input = ({
         placeholder="Enter a verb"
         value={searchVerb}
         onChange={handleSearchChange}
+        onClick={handleInputClick}
+        key={searchVerb}
         className="bg-slate-100 placeholder:text-fuchsia-800 text-fuchsia-800 w-full md:w-72 h-10 pl-3 mb-2 md:mr-2 rounded focus:outline-none tracking-wider"
       />
       <Dropdown
